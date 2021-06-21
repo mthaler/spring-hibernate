53e744c8a6f2cfa800d9b1548ce3428d4e9013b6
Michael Thaler <michael.thaler@dlr.de>
2021-06-21 16:20:16 +0200

    Added build files.

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.5.1"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.5.10"
+	kotlin("plugin.spring") version "1.5.10"
+}
+
+group = "com.mthaler"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework:spring-context:5.3.8")
+	implementation("org.springframework:spring-jdbc:5.3.8")
+	implementation("ch.qos.logback:logback-classic:1.3.0-alpha5")
+	implementation("javax.annotation:javax.annotation-api:1.3.2")
+	implementation("org.apache.commons:commons-dbcp2:2.8.0")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("junit:junit:4.13.1")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation(kotlin("test"))
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
