270de159c499884018eb81e8eaee4835b29663c7
Michael Thaler <michael.thaler@dlr.de>
2021-06-21 16:46:14 +0200

    Added InstrumentDaoImpl.

---
+++ src/main/kotlin/com/mthaler/springhibernate/dao/InstrumentDaoImpl.kt
@@ -0,0 +1,30 @@
+package com.mthaler.springhibernate.dao
+
+import com.mthaler.springhibernate.entities.Instrument
+import org.apache.commons.logging.LogFactory
+import org.hibernate.SessionFactory
+import javax.annotation.Resource
+
+class InstrumentDaoImpl: InstrumentDao {
+
+    private lateinit var sessionFactory: SessionFactory
+
+    fun getSessionFactory(): SessionFactory {
+        return sessionFactory
+    }
+
+    @Resource(name = "sessionFactory")
+    fun setSessionFactory(sessionFactory: SessionFactory?) {
+        this.sessionFactory = sessionFactory!!
+    }
+
+    override fun save(instrument: Instrument): Instrument {
+        sessionFactory.getCurrentSession().saveOrUpdate(instrument);
+        logger.info("Instrument saved with id: " + instrument.instrumentId);
+        return instrument;
+    }
+
+    companion object {
+        private val logger = LogFactory.getLog(InstrumentDaoImpl::class.java)
+    }
+}
