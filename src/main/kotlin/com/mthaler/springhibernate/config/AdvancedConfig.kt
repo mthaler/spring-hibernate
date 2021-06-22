612af1e6b1b9d171fb16e0b63535fdc367c37451
Michael Thaler <michael.thaler@dlr.de>
2021-06-22 09:10:05 +0200

    Updated AdvancedConfig.

--- src/main/kotlin/com/mthaler/springhibernate/config/AdvancedConfig.kt
+++ src/main/kotlin/com/mthaler/springhibernate/config/AdvancedConfig.kt
@@ -54,8 +54,7 @@ class AdvancedConfig {
 
     private fun hibernateProperties(): Properties {
         val hibernateProp = Properties()
-        hibernateProp["hibernate.dialect"] = "org.hibernate.dialect.H2Dialect"
-        hibernateProp["hibernate.hbm2ddl.auto"] = "create-drop"
+        hibernateProp["hibernate.dialect"] = "org.hibernate.dialect.PostgreSQL9Dialect"
         hibernateProp["hibernate.format_sql"] = true
         hibernateProp["hibernate.use_sql_comments"] = true
         hibernateProp["hibernate.show_sql"] = true
